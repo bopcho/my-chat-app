bd7f02a320f35d46046b09cf2b4bc99981952a54
bopcho <ivanoffboyan@gmail.com>
2023-11-10 23:00:36 +0100

    Update gh-pages with dist contents

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: 'src/App.jsx', // Update with your actual entry file
+    },
+  },
+});
